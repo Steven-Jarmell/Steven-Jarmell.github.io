5a3cc9b2e05fe8c3534016c6289ca698710d09ef
Steven-Jarmell <sjj27@pitt.edu>
2023-05-02 15:41:41 -0400

    Create and style main content

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -1,9 +1,22 @@
 import '../Styles/MainContent.css';
+import { BsArrowReturnRight } from "react-icons/bs";
+import Astronaut from '../assets/astronaut.png';
 
 const MainContent = () => {
     return (
         <div className="MainContent__container">
-            <h1>Main Content</h1>
+            <div className="MainContent__info">
+                <p>Hey,</p>
+                <p>My name is Steven Jarmell, I'm a Student at the <b><span className="MainContent__info-pitt">University of Pittsburgh</span></b> and a <b>Full-Stack Software Engineer</b>.</p>
+                
+                <div className="MainContent__info-project-link">
+                    <p>Check out my projects</p>
+                    <BsArrowReturnRight />
+                </div>
+            </div>
+            <div className="MainContent__picture-container">
+                <img src={Astronaut} alt='Astronaut on Computer' className="MainContent__picture"/>
+            </div>
         </div>
     )
 }
