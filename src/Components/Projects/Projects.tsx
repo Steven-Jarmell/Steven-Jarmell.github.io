02a23fb4e8894d1c6a7031c3cf3eeef9ff4c9ce0
Steven-Jarmell <sjj27@pitt.edu>
2023-05-03 15:17:48 -0400

    Add Beefin and Wordle Clone to projects

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,5 +1,5 @@
-import "../../Styles/Projects.css"
-import Project from './Project';
+import "../../Styles/Projects.css";
+import Project from "./Project";
 import CSC_Internship_Repo from "../../assets/csc-internship-repo.jpg";
 import Beefin from "../../assets/beefin.jpg";
 import Big_Bens from "../../assets/big-bens.jpg";
@@ -9,20 +9,64 @@ import Reddit_Scraper from "../../assets/reddit-scraper.jpg";
 import Tux from "../../assets/tux.jpg";
 import Wordle from "../../assets/wordle.jpg";
 import Wiki_Bot from "../../assets/wiki-bot.jpg";
-import BeSocial from "../../assets/besocial.jpg"
+import BeSocial from "../../assets/besocial.jpg";
 
-const CSCWebsiteDescription = "Designed and implemented a proof-of-concept website to replace the original CSC Internship Repository. Allows users to sign in with GitHub OAuth, add new jobs, as well as filter existing jobs. Jobs that are added get sent to an admin only dashboard where they are reviewed before being published.";
+const CSCWebsiteDescription: string =
+    "Designed and implemented a proof-of-concept website to replace the original CSC Internship Repository. Allows users to sign in with GitHub OAuth, add new jobs, as well as filter existing jobs. All newly added job postings undergo a rigorous review process by an admin before being published. The platform offers a more efficient, streamlined way to connect aspiring interns with potential employers.";
+const BeefinDescription: string =
+    "Collaborative project which created a full-stack social media workout app, leveraging a microservice backend architecture based on Java Spring Boot. We seamlessly connected the backend to the frontend using RESTful APIs and Json Web Tokens, resulting in a robust, secure, and scalable application.";
+const WordleDescription: string =
+    "Created a clone of the popular game 'Wordle' while learning React and TypeScript. In addition to the core game mechanics, I added a feature to allow for multiple games to be played daily, enhancing the user experience.";
+const RedditScraperDescription: string = 
+    ""
 
 const Projects = () => {
     return (
         <div className="projects__element-container">
-
             <div className="projects__container">
                 {/* CSC Internship Repo */}
-                <Project imageSrc={CSC_Internship_Repo} name="CSC Internship Website" description={CSCWebsiteDescription} keywords={["TypeScript", "React", "Express", "Node", "MongoDB"]} link="https://github.com/Steven-Jarmell/CSC-Internships"/>
+                <Project
+                    imageSrc={CSC_Internship_Repo}
+                    name="CSC Internship Website"
+                    description={CSCWebsiteDescription}
+                    keywords={[
+                        "TypeScript",
+                        "React",
+                        "Express",
+                        "Node",
+                        "MongoDB",
+                        "Figma",
+                        "Personal Project",
+                    ]}
+                    link="https://github.com/Steven-Jarmell/CSC-Internships"
+                />
+
                 {/* Beefin */}
+                <Project
+                    imageSrc={Beefin}
+                    name="Beefin"
+                    description={BeefinDescription}
+                    keywords={[
+                        "Java",
+                        "Spring Boot",
+                        "JavaScript",
+                        "React",
+                        "Firebase",
+                        "Docker",
+                        "JWT",
+                        "Personal Project",
+                    ]}
+                    link="https://github.com/Steven-Jarmell/Beefin"
+                />
 
                 {/* Wordle Clone */}
+                <Project
+                    imageSrc={Wordle}
+                    name="Wordle Clone"
+                    description={WordleDescription}
+                    keywords={["TypeScript", "React", "Personal Project"]}
+                    link="https://github.com/Steven-Jarmell/Wordle-Clone"
+                />
 
                 {/* Reddit Scraper */}
 
@@ -43,7 +87,7 @@ const Projects = () => {
                 {/* Wikipedia Bot */}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Projects;
