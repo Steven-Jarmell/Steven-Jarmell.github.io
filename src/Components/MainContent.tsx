608cae62737624621b38ed196e31425797e099f5
Steven Jarmell <sjj27@pitt.edu>
2024-01-09 11:22:19 -0500

    Update bio

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -13,13 +13,13 @@ const MainContent = () => {
                 <div className="MainContent__info">
                     <p>Hey,</p>
                     <p>
-                        My name is Steven Jarmell, I'm a Student at the{" "}
+                        My name is Steven Jarmell. I'm a {" "}
                         <b>
                             <span className="MainContent__info-pitt">
                                 University of Pittsburgh
                             </span>
                         </b>{" "}
-                        and a <b>Full-Stack Software Engineer</b>.
+                        graduate and a <b>Software Engineer at Amazon Web Services</b>.
                     </p>
 
                     <div
